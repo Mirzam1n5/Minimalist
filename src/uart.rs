acd9368a4d1b98ae9c830dac93558520b95c27d7
Mirzam1n5 <milanamelnikova1307@gmail.com>
2026-04-06 19:36:34 +0500

    getting rid of unnecessary raw pointers

--- src/uart.rs
+++ src/uart.rs
@@ -1,3 +1,5 @@
+use core::fmt::{Error, Write};
+
 pub struct Uart {
     base_address: usize,
 }
@@ -16,5 +18,32 @@ impl Uart {
             ptr.add(2).write_volatile(1);
             ptr.add(1).write_volatile(1);
         }
+    }
+
+    pub fn put(&mut self, c: u8) {
+        let ptr = self.base_address as *mut u8;
+        unsafe {
+            ptr.add(0).write_volatile(c);
+        }
+    }
+
+    pub fn get(&mut self) -> Option<u8> {
+        let ptr = self.base_address as *mut u8;
+        unsafe {
+            if ptr.add(5).read_volatile() & 1 == 0 {
+                None
+            } else {
+                Some(ptr.add(0).read_volatile())
+            }
+        } 
+    }
+}
+
+impl Write for Uart {
+    fn write_str(&mut self, out: &str) -> Result<(), Error> {
+        for c in out.bytes() {
+            self.put(c);
+        }
+        Ok(())
     }
 }
