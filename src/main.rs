561bb4d8064b8933e9c670cb51141240aab1ca96
Mirzam1n5 <milanamelnikova1307@gmail.com>
2026-04-06 19:40:04 +0500

    fixed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
-use minimalist::kmain; 
+use core::arch::global_asm;
+
+global_asm!(include_str!("asm/boot.S")); 
+global_asm!(include_str!("asm/trap.S"));
+
+mod uart;
 
 #[unsafe(no_mangle)]
-extern "C" fn _start() -> ! {
-    kmain();
+pub extern "C" fn kmain() -> ! {
+    let mut my_uart = uart::Uart::new(0x1000_0000);
+    my_uart.init();
+
+    use core::fmt::Write;
+    let _ = write!(my_uart, "Hello from Rust kmain!\n");
+
+    loop { }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop { }
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn abort() -> ! {
+    loop { }
 }
